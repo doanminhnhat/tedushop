462baef01f0a62c23030eab4fecdef5e95a8e6ab
doanminhnhat <doanminhnhatnhat@gmail.com>
2018-02-22 12:43:37 +0700

    add more repositories for other tables

---
+++ TeduShop.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Model;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IFooterRepositor 
+    { 
+
+    }
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepositor
+    {
+        public FooterRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/VisitorStatisticRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Model;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IVisitorStatisticRepository
+    {
+
+    }
+    public class VisitorStatisticRepository : RepositoryBase<VisitorStatistic>, IVisitorStatisticRepository
+    {
+        public VisitorStatisticRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+
+        }
+    }
+}
